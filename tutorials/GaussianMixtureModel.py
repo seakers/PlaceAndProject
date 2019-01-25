6d018bef98967b861bc3fd0601d99beefc61ce15
Nathan Knerr <importN@hotmail.com>
2019-01-25 12:04:09 -0600

    Added GMM and Kriging Tutorials

---
+++ tutorials/GaussianMixtureModel.py
@@ -0,0 +1,68 @@
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.12-gaussian-mixtures.html
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+
+# Generate some data
+from sklearn.datasets.samples_generator import make_blobs
+X, y_true = make_blobs(n_samples=400, centers=4,
+                       cluster_std=0.60, random_state=0)
+X = X[:, ::-1] # flip axes for better plotting
+
+# Plot the data with K Means Labels
+from sklearn.cluster import KMeans
+kmeans = KMeans(4, random_state=0)
+labels = kmeans.fit(X).predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis');
+
+plt.show()
+
+# the GMM
+from sklearn.mixture import GMM
+gmm = GMM(n_components=4).fit(X)
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis');
+
+plt.show()
+
+probs = gmm.predict_proba(X)
+print(probs[:5].round(3))
+from matplotlib.patches import Ellipse
+
+
+def draw_ellipse(position, covariance, ax=None, **kwargs):
+    """Draw an ellipse with a given position and covariance"""
+    ax = ax or plt.gca()
+
+    # Convert covariance to principal axes
+    if covariance.shape == (2, 2):
+        U, s, Vt = np.linalg.svd(covariance)
+        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width, height = 2 * np.sqrt(s)
+    else:
+        angle = 0
+        width, height = 2 * np.sqrt(covariance)
+
+    # Draw the Ellipse
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position, nsig * width, nsig * height,
+                             angle, **kwargs))
+
+
+def plot_gmm(gmm, X, label=True, ax=None):
+    ax = ax or plt.gca()
+    labels = gmm.fit(X).predict(X)
+    if label:
+        ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis', zorder=2)
+    else:
+        ax.scatter(X[:, 0], X[:, 1], s=40, zorder=2)
+    ax.axis('equal')
+
+    w_factor = 0.2 / gmm.weights_.max()
+    for pos, covar, w in zip(gmm.means_, gmm.covars_, gmm.weights_):
+        draw_ellipse(pos, covar, alpha=w * w_factor)
+
+rng = np.random.RandomState(13)
+X_stretched = np.dot(X, rng.randn(2, 2))
+gmm = GMM(n_components=4, covariance_type='full', random_state=42)
+plot_gmm(gmm, X_stretched)
