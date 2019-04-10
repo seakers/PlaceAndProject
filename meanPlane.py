Removed redundant and non-functional Legendre Analytics. Renamed V in mean plane. Fixed calculation error in tradeoffMatrixImage.

meanPlane.py
@@ -20,18 +20,18 @@ class MeanPlane():
         self.paretoSamples=paretoSamples
         self._centeredSamples=paretoSamples-self.meanPoint
         self.embedDim=paretoSamples.shape[1]
-        self._U, self._S, self._V=np.linalg.svd(self._centeredSamples)
+        self._U, self._S, self._Vt=np.linalg.svd(self._centeredSamples)
 
     @property
     def normalVect(self):
         """
         :return: the normalized normal vector to the plane
         """
-        return self._V[-1,:]
+        return self._Vt[-1, :]
 
     @property
     def basisVects(self):
-        return self._V[:-1,:]
+        return self._Vt[:-1, :]
 
     @property
     def projectionToPlaneMat(self): # I do believe this is the same as basisVects actually
@@ -173,7 +173,7 @@ class ParetoMeanPlane(MeanPlane):
     def __init__(self,paretoSamples):
         super(ParetoMeanPlane,self).__init__(paretoSamples)
         if np.all(self.normalVect<0):
-            self._V*=-1 # default to pointing out--positive
+            self._Vt*=-1 # default to pointing out--positive
             self._U*=-1
         elif np.any(self.normalVect<0): # if not all negative or positive
             raise NotPointingToOriginError(self.normalVect)

polyfitting/polycommon.py
@@ -11,7 +11,7 @@ import common as cmn
 class PolynomialAnalyzer():
     """
     """
-    def __init__(self,forwardTransform, reconstruct, reconstructDerivative, pointHeight,pointLocation,ordersToEval=None, normalizeMin=None, normalizeRange=None):
+    def __init__(self,forwardTransform, reconstruct, reconstructDerivative, reconstructHessian, pointHeight,pointLocation,ordersToEval=None, normalizeMin=None, normalizeRange=None):
         self.pointHeight=pointHeight
         self.forwardTransform=forwardTransform
         self.pointLocation=pointLocation
@@ -26,6 +26,7 @@ class PolynomialAnalyzer():
         self.normalizedPointLocation=self.normalize(pointLocation)
         self.reconCall=reconstruct
         self.reconDcall=reconstructDerivative
+        self.reconHcall=reconstructHessian
         if ordersToEval is None:
             if len(pointLocation.shape)==1:
                 self.ordersToEval=cmn.numpyze(self.pointHeight.size-1)
@@ -92,6 +93,13 @@ class PolynomialAnalyzer():
         else:
             locations=self.normalize(locations)
         return self.reconDcall(self.ordersToEval, locations, self.filteredSpectrum(), self.pointHeight.size)
+
+    def reconstructHessian(self, locations=None):
+        if locations is None:
+            locations=self.normalizedPointLocation
+        else:
+            locations=self.normalize(locations)
+        return self.reconHcall(self.ordersToEval, locations, self.filteredSpectrum(), self.pointHeight.size)
 
     def avgSqrdReconstructionError(self):
         return np.mean((self.reconstruction()-self.pointHeight)**2)

tradeoffMatrixImage.py
@@ -66,7 +66,7 @@ def tradeoffCalcSingleLoc(meanPlane, fourierAnalyzer,loc,obj1,obj2):
     else:
         dirToTravel=np.array([1,])[:,np.newaxis]
     moveInY=np.real(np.dot(gradYwrtX,dirToTravel)) # should be real
-    return -moveInY[obj1]/moveInY[obj2]
+    return -moveInY[obj2]/moveInY[obj1]
 
 def tradeoffCalc(meanPlane, fourierAnalyzer,locations,obj1,obj2):
     accum=[]